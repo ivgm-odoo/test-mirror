40aafe7779f66695dc22443225c9e5f9e834a444
yuro-odoo <108021115+yuro-odoo@users.noreply.github.com>
2023-05-17 16:18:30 -0700

    16.0 3322799 fix yuro (#11)
    * 16.0 3267173 fix yuro (#4)
    
    * [FIX]fixed supervisor can't receive todo notification error, fixed action_reject to send todo task to correct user and make approval process can be repeated based on dev spec
    
    * [FIX]changed note in todo
    
    * [FIX] removed multi
    
    ---------
    
    Co-authored-by: aal-odoo <60622764+aal-odoo@users.noreply.github.com>
    
    * 16.0 3267173 fix yuro (#7)
    
    * [FIX]easyred_field_service: changed notification sent to assignees' managers and todo notification sent to assignees
    
    * version bump
    
    * [FIX]fixed could not import pdf417gen error
    
    * [REBUILD]
    
    * [FIX]added service_id in stock picking form
    
    ---------
    
    Co-authored-by: Keyur Gajjar <kga@odoo.com>
    Co-authored-by: aal-odoo <60622764+aal-odoo@users.noreply.github.com>
    Co-authored-by: jimm-odoo <108091042+jimm-odoo@users.noreply.github.com>

--- easyred_field_service/__manifest__.py
+++ easyred_field_service/__manifest__.py
@@ -30,7 +30,7 @@ The warehouse receives requests for materials associated with the task.
     'author': "Odoo, Inc",
     'website': "http://www.odoo.com",
     'category': 'Custom Developments',
-    'version': '0.2',
+    'version': '0.3',
     'license': 'OPL-1',
     'depends': [
         'project',
@@ -41,6 +41,7 @@ The warehouse receives requests for materials associated with the task.
     'data': [
         'security/ir.model.access.csv',
         'security/approval_group.xml',
-        'views/field_service_form.xml'
+        'views/field_service_form.xml',
+        'views/stock_picking_form_view.xml'
     ],
 }
