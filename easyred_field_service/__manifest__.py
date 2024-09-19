19a5078643fae6e7aaacd1aa88aac439a706ed6b
ivgm-odoo <ivgm@odoo.com>
2024-09-19 13:58:28 -0600

    [UPG][4006137] run pre-commit
    Fix small errors detected by pre-commit
    easyred_field_service/models/task.py:44:85: E712 Avoid equality comparisons to `False`; use `if not self.to_supervisor:` for false checks
    easyred_field_service/models/material.py:9: [W8113(attribute-string-redundant), FsmMaterial] The attribute string is redundant. String parameter equal to name of variable
    easyred_field_service/tests/common.py:8:14: UP008 Use `super()` instead of `super(__class__, self)`
    easyred_field_service/__manifest__.py:27: [C8101(manifest-required-author), ] One of the following authors must be present in manifest: 'Odoo PS'
    easyred_field_service/__manifest__.py:4: [C8103(manifest-deprecated-key), ] Deprecated key "description" in manifest file
    easyred_field_service/__manifest__.py:31: [C8105(license-allowed), ] License "OPL-1" not allowed in manifest file.
    easyred_field_service/__manifest__.py:1: [C8112(missing-readme), ] Missing ./README.rst file. Template here: https://github.com/OCA/maintainer-tools/blob/master/template/module/README.rst

--- easyred_field_service/__manifest__.py
+++ easyred_field_service/__manifest__.py
@@ -1,47 +1,15 @@
 {
-    'name': "Easyred field service",
-
-    'summary': "Easyred field service",
-
-    'description': """
-task id: 3054030
-The client is a company that sells field service projects, and they currently 
-handle two types of processes that are essential for them to exist in Odoo, 
-this dev request is for steps 1-7:
-
-Scenario 1
-
-The final customer submits a ticket with a specific requirement (installation/maintenance service).
-
-Request enters ODOO.
-
-The contract manager follows up on the ticket.
-
-From the ticket, the contract manager creates the task, designates technicians, and adds the materials that are needed.
-
-The contract Manager sends the material request for approval.
-
-The supervisor approves or rejects the material request.
-
-The warehouse receives requests for materials associated with the task.
-
-    """,
-
-    'author': "Odoo, Inc",
-    'website': "http://www.odoo.com",
-    'category': 'Custom Developments',
-    'version': '17.0.1.0.0',
-    'license': 'OPL-1',
-    'depends': [
-        'project',
-        'product',
-        'industry_fsm_sale',
-        'stock'
-    ],
-    'data': [
-        'security/ir.model.access.csv',
-        'security/approval_group.xml',
-        'views/field_service_form.xml',
-        'views/stock_picking_form_view.xml'
+    "name": "Easyred field service",
+    "author": "Odoo PS",
+    "website": "https://www.odoo.com",
+    "category": "Custom Developments",
+    "version": "17.0.1.0.0",
+    "license": "OEEL-1",
+    "depends": ["industry_fsm_sale", "stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/approval_group.xml",
+        "views/field_service_form.xml",
+        "views/stock_picking_form_view.xml",
     ],
 }
--- easyred_field_service/tests/common.py
+++ easyred_field_service/tests/common.py
@@ -1,33 +1,30 @@
 from odoo.addons.industry_fsm_sale.tests.common import TestFsmFlowCommon
 
+
 class TestEasyredFsmCommon(TestFsmFlowCommon):
-    
     @classmethod
     def setUpClass(cls):
-        super(TestEasyredFsmCommon, cls).setUpClass()
+        super().setUpClass()
 
-        cls.project_admin = cls.env['res.users'].create({
-            'name': 'Test Project_admin',
-            'login': 'Test',
-            'email': 'test.project_admin@example.com',
-            'groups_id': [(6, 0, [cls.env.ref('easyred_field_service.group_task_approval').id])]
+        cls.project_admin = cls.env["res.users"].create({
+            "name": "Test Project_admin",
+            "login": "Test",
+            "email": "test.project_admin@example.com",
+            "groups_id": [(6, 0, [cls.env.ref("easyred_field_service.group_task_approval").id])],
         })
 
-        cls.partner_a = cls.env['res.partner'].create({
-            'name': 'partner_a',
-            'company_id': False,
-        })
-        
-        cls.task.write({
-            'partner_id': cls.partner_a,
-            'is_saleorder': False
+        cls.partner_a = cls.env["res.partner"].create({
+            "name": "partner_a",
+            "company_id": False,
         })
 
-        cls.product_delivered = cls.env['product.product'].create({
-            'name': 'Consommable product delivery',
-            'list_price': 40,
-            'detailed_type': 'product',
-            'invoice_policy': 'delivery',
-            'qty_available': 20,
-            'uom_id': cls.env.ref('uom.product_uom_unit').id,
+        cls.task.write({"partner_id": cls.partner_a, "is_saleorder": False})
+
+        cls.product_delivered = cls.env["product.product"].create({
+            "name": "Consommable product delivery",
+            "list_price": 40,
+            "detailed_type": "product",
+            "invoice_policy": "delivery",
+            "qty_available": 20,
+            "uom_id": cls.env.ref("uom.product_uom_unit").id,
         })
